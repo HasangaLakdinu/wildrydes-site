500aeb75663847f87547adfa3849f1757127de30
Hasanga Lakdinu <36729333+HasangaLakdinu@users.noreply.github.com>
2025-07-13 13:12:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_jZNY8wDSb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '42af9j36u561k94q7a3360d6im', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
